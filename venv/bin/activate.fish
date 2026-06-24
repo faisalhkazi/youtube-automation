ae5ee56acdcad8f53a6149f9fadd4c7572f4bfbe
Faisal Kazi <faislahkazi@gmail.com>
2026-06-24 09:28:37 +0000

    Audio generating fix

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/rocky/venv
+set -gx VIRTUAL_ENV /home/rocky/youtube-project/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
--- youtube-automation/app.py
+++ youtube-automation/app.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Request, Form
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+from datetime import datetime
+import subprocess
 
 app = FastAPI()
 
@@ -8,6 +11,7 @@ templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 async def home(request: Request):
+
     return templates.TemplateResponse(
         request=request,
         name="index.html"
@@ -16,12 +20,39 @@ async def home(request: Request):
 
 @app.post("/generate")
 async def generate(
+    request: Request,
     title: str = Form(...),
     category: str = Form(...),
     script: str = Form(...)
 ):
-    return {
-        "title": title,
-        "category": category,
-        "script_length": len(script)
-    }
+
+    timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+
+    script_file = f"scripts/{timestamp}.txt"
+    audio_file = f"audio/{timestamp}.wav"
+
+    with open(script_file, "w") as f:
+        f.write(script)
+
+    command = f"""
+cat {script_file} | \
+/home/rocky/youtube-project/piper/piper/piper \
+--model /home/rocky/youtube-project/piper/models/en_US-lessac-medium.onnx \
+--output_file {audio_file}
+"""
+
+    subprocess.run(command, shell=True)
+
+    return HTMLResponse(f"""
+    <h2>Audio Generated Successfully</h2>
+
+    <p>Title: {title}</p>
+
+    <p>Category: {category}</p>
+
+    <p>Audio File:</p>
+
+    <pre>{audio_file}</pre>
+
+    <a href="/">Back</a>
+    """)
